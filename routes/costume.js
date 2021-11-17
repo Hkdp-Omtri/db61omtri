13d5d779b4151786a96bd0611d21ab57e3f7eaff
Hkdp_Omtri <s544929@nwmissouri.edu>
2021-11-17 16:34:13 -0600

    files to repo

--- routes/costume.js
+++ routes/costume.js
@@ -9,3 +9,8 @@ module.exports = router;
 /* GET detail costume page */
 router.get('/detail', costume_controlers.costume_view_one_Page);
 
+/* GET create costume page */
+router.get('/create', costume_controlers.costume_create_Page);
+
+/* GET create update page */
+router.get('/update', costume_controlers.costume_update_Page);
