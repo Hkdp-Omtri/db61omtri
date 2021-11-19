54b0322f877579751cdd693b9f8dbca6eed91927
Hkdp_Omtri <s544929@nwmissouri.edu>
2021-11-18 21:16:08 -0600

    files to repo for lab

--- app.js
+++ app.js
@@ -3,7 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }));
+
+
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
 
 const connectionString = process.env.MONGO_CON 
 mongoose = require('mongoose'); 
@@ -27,6 +47,7 @@ var selectorRouter = require('./routes/selector');
 const Costume = require("./models/costume");
 const resoureRouter = require('./routes/resource');
 var costumeRouter = require('./routes/costume');
+var Account =require('./models/account'); 
 
 var app = express();
 
@@ -86,5 +107,19 @@ async function recreateDB(){
   });
   // let reseed = true; 
   // if (reseed) { recreateDB();}  
+
+  app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  //model account
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 } 
 module.exports = app;
